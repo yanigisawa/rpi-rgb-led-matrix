bb426140f1187de009fe49499b0d807914d06abd
James Alexander <github@jamesralexander.com>
2017-02-26 19:04:25 +0000

    - Animate a jumping Mario, with alternating directions

--- python/samples/mario.py
+++ python/samples/mario.py
@@ -27,35 +27,42 @@ class Mario(SampleBase):
 
     def run(self):
         x, y, step = 0, 0, 1
+        count = 0
         self.canvas = self.matrix.CreateFrameCanvas()
+        inverse = False
         while True:
             #     offset_canvas.SetPixel(offset_canvas.height - 1 - x, x, 255, 0, 255)
 
             self.clearScreen()
-            self.draw(self.getStanding(x, y))
-            # self.draw(self.getJumping(x, y))
+            if count % 2 == 0:
+                self.draw(self.getStanding(x, y, inverse))
+            else:
+                # self.draw(self.getStanding(x, y, inverse))
+                self.draw(self.getJumping(x, y, inverse))
 
             time.sleep(0.2)
             x += step
-            if x > 20 or x < 0:
+            if x > 31 or x < -14:
                 step *= -1
+                inverse = not inverse
 
+            count += 1
             self.canvas = self.matrix.SwapOnVSync(self.canvas)
 
     def draw(self, pixels):
         for p in pixels:
             self.canvas.SetPixel(p.x, p.y, p.red, p.green, p.blue)
 
-    def getJumping(self, x, y):
+    def getJumping(self, x, y, inverse = False):
         pixels = []
         pixels.append(Pixel(x + 4,  y + 0, *self.red))
         pixels.append(Pixel(x + 5,  y + 0, *self.red))
         pixels.append(Pixel(x + 6,  y + 0, *self.red))
         pixels.append(Pixel(x + 7,  y + 0, *self.red))
         pixels.append(Pixel(x + 8,  y + 0, *self.red))
-        pixels.append(Pixel(x + 10,  y + 0, *self.flesh) )
         pixels.append(Pixel(x + 11,  y + 0, *self.flesh) )
         pixels.append(Pixel(x + 12,  y + 0, *self.flesh) )
+        pixels.append(Pixel(x + 13,  y + 0, *self.flesh) )
 
         pixels.append(Pixel(x + 3,  y + 1, *self.red))
         pixels.append(Pixel(x + 4,  y + 1, *self.red))
@@ -66,6 +73,8 @@ class Mario(SampleBase):
         pixels.append(Pixel(x + 9,  y + 1, *self.red))
         pixels.append(Pixel(x + 10, y + 1, *self.red))
         pixels.append(Pixel(x + 11, y + 1, *self.red))
+        pixels.append(Pixel(x + 12,  y + 1, *self.flesh) )
+        pixels.append(Pixel(x + 13,  y + 1, *self.flesh) )
 
         # Head / Hair)
         pixels.append(Pixel(x + 3,  y + 2, *self.brown))
@@ -76,6 +85,9 @@ class Mario(SampleBase):
         pixels.append(Pixel(x + 7,  y + 2, *self.flesh) )
         pixels.append(Pixel(x + 8,  y + 2, *self.brown))
         pixels.append(Pixel(x + 9,  y + 2, *self.flesh))
+        pixels.append(Pixel(x + 11, y + 2, *self.red))
+        pixels.append(Pixel(x + 12, y + 2, *self.red))
+        pixels.append(Pixel(x + 13, y + 2, *self.red))
 
         pixels.append(Pixel(x + 2,  y + 3, *self.brown))
         pixels.append(Pixel(x + 3,  y + 3, *self.flesh))
@@ -85,8 +97,11 @@ class Mario(SampleBase):
         pixels.append(Pixel(x + 7,  y + 3, *self.flesh) )
         pixels.append(Pixel(x + 8,  y + 3, *self.brown))
         pixels.append(Pixel(x + 9,  y + 3, *self.flesh))
-        pixels.append(Pixel(x + 10, y +  3, *self.flesh))
-        pixels.append(Pixel(x + 11, y +  3,*self.flesh))
+        pixels.append(Pixel(x + 10, y + 3, *self.flesh))
+        pixels.append(Pixel(x + 11, y + 3,*self.flesh))
+        pixels.append(Pixel(x + 12, y + 3, *self.red))
+        pixels.append(Pixel(x + 13, y + 3, *self.red))
+
 
 
         pixels.append(Pixel(x + 2,  y + 4, *self.brown))
@@ -97,9 +112,10 @@ class Mario(SampleBase):
         pixels.append(Pixel(x + 7,  y + 4, *self.flesh))
         pixels.append(Pixel(x + 8,  y + 4, *self.flesh))
         pixels.append(Pixel(x + 9,  y + 4, *self.brown))
-        pixels.append(Pixel(x + 10, y +  4, *self.flesh))
-        pixels.append(Pixel(x + 11, y +  4, *self.flesh))
-        pixels.append(Pixel(x + 12, y +  4, *self.flesh))
+        pixels.append(Pixel(x + 10, y + 4, *self.flesh))
+        pixels.append(Pixel(x + 11, y + 4, *self.flesh))
+        pixels.append(Pixel(x + 12, y + 4, *self.flesh))
+        pixels.append(Pixel(x + 13, y + 4, *self.red))
 
 
         pixels.append(Pixel(x + 2,  y + 5, *self.brown))
@@ -112,6 +128,7 @@ class Mario(SampleBase):
         pixels.append(Pixel(x + 9,  y + 5, *self.brown))
         pixels.append(Pixel(x + 10, y +  5, *self.brown))
         pixels.append(Pixel(x + 11, y +  5, *self.brown))
+        pixels.append(Pixel(x + 12, y + 5, *self.red))
 
 
         pixels.append(Pixel(x + 3, y + 6, *self.flesh))
@@ -121,115 +138,118 @@ class Mario(SampleBase):
         pixels.append(Pixel(x + 7, y + 6, *self.flesh))
         pixels.append(Pixel(x + 8, y + 6, *self.flesh))
         pixels.append(Pixel(x + 9, y + 6, *self.flesh))
+        pixels.append(Pixel(x + 10, y + 6, *self.red))
+        pixels.append(Pixel(x + 11, y + 6, *self.red))
 
 
         # Body)
+        pixels.append(Pixel(x - 2, y + 7, *self.flesh))
+        pixels.append(Pixel(x - 1, y + 7, *self.flesh))
+        pixels.append(Pixel(x + 0, y + 7, *self.flesh))
+        pixels.append(Pixel(x + 1, y + 7, *self.red))
         pixels.append(Pixel(x + 2, y + 7, *self.red))
         pixels.append(Pixel(x + 3, y + 7, *self.red))
-        pixels.append(Pixel(x + 4, y + 7, *self.blue))
-        pixels.append(Pixel(x + 5, y + 7, *self.red))
+        pixels.append(Pixel(x + 4, y + 7, *self.red))
+        pixels.append(Pixel(x + 5, y + 7, *self.blue))
         pixels.append(Pixel(x + 6, y + 7, *self.red))
         pixels.append(Pixel(x + 7, y + 7, *self.red))
         pixels.append(Pixel(x + 8, y + 7, *self.red))
+        pixels.append(Pixel(x + 9, y + 7, *self.blue))
+        pixels.append(Pixel(x + 10, y + 7, *self.red))
+        pixels.append(Pixel(x + 11, y + 7, *self.red))
+        pixels.append(Pixel(x + 14, y + 7, *self.brown))
 
 
+        pixels.append(Pixel(x - 2, y + 8, *self.flesh))
+        pixels.append(Pixel(x - 1, y + 8, *self.flesh))
+        pixels.append(Pixel(x + 0, y + 8, *self.flesh))
         pixels.append(Pixel(x + 1, y + 8, *self.red))
         pixels.append(Pixel(x + 2, y + 8, *self.red))
         pixels.append(Pixel(x + 3, y + 8, *self.red))
-        pixels.append(Pixel(x + 4, y + 8, *self.blue))
+        pixels.append(Pixel(x + 4, y + 8, *self.red))
         pixels.append(Pixel(x + 5, y + 8, *self.red))
-        pixels.append(Pixel(x + 6, y + 8, *self.red))
-        pixels.append(Pixel(x + 7, y + 8, *self.blue))
+        pixels.append(Pixel(x + 6, y + 8, *self.blue))
+        pixels.append(Pixel(x + 7, y + 8, *self.red))
         pixels.append(Pixel(x + 8, y + 8, *self.red))
         pixels.append(Pixel(x + 9, y + 8, *self.red))
-        pixels.append(Pixel(x + 10, y + 8, *self.red))
+        pixels.append(Pixel(x + 10, y + 8, *self.blue))
+        pixels.append(Pixel(x + 13, y + 8, *self.brown))
+        pixels.append(Pixel(x + 14, y + 8, *self.brown))
 
 
-        pixels.append(Pixel(x + 0, y + 9, *self.red))
-        pixels.append(Pixel(x + 1, y + 9, *self.red))
-        pixels.append(Pixel(x + 2, y + 9, *self.red))
+        pixels.append(Pixel(x - 1, y + 9, *self.flesh))
         pixels.append(Pixel(x + 3, y + 9, *self.red))
-        pixels.append(Pixel(x + 4, y + 9, *self.blue))
-        pixels.append(Pixel(x + 5, y + 9, *self.blue))
+        pixels.append(Pixel(x + 4, y + 9, *self.red))
+        pixels.append(Pixel(x + 5, y + 9, *self.red))
         pixels.append(Pixel(x + 6, y + 9, *self.blue))
         pixels.append(Pixel(x + 7, y + 9, *self.blue))
-        pixels.append(Pixel(x + 8, y + 9, *self.red))
-        pixels.append(Pixel(x + 9, y + 9, *self.red))
-        pixels.append(Pixel(x + 10, y + 9, *self.red))
-        pixels.append(Pixel(x + 11, y + 9, *self.red))
+        pixels.append(Pixel(x + 8, y + 9, *self.blue))
+        pixels.append(Pixel(x + 9, y + 9, *self.blue))
+        pixels.append(Pixel(x + 10, y + 9, *self.yellow))
+        pixels.append(Pixel(x + 11, y + 9, *self.blue))
+        pixels.append(Pixel(x + 12, y + 9, *self.blue))
+        pixels.append(Pixel(x + 13, y + 9, *self.brown))
+        pixels.append(Pixel(x + 14, y + 9, *self.brown))
 
 
-        pixels.append(Pixel(x + 0, y + 10, *self.flesh))
-        pixels.append(Pixel(x + 1, y + 10, *self.flesh))
-        pixels.append(Pixel(x + 2, y + 10, *self.red))
-        pixels.append(Pixel(x + 3, y + 10, *self.blue))
-        pixels.append(Pixel(x + 4, y + 10, *self.yellow))
+        pixels.append(Pixel(x + 4, y + 10, *self.blue))
         pixels.append(Pixel(x + 5, y + 10, *self.blue))
         pixels.append(Pixel(x + 6, y + 10, *self.blue))
         pixels.append(Pixel(x + 7, y + 10, *self.yellow))
         pixels.append(Pixel(x + 8, y + 10, *self.blue))
-        pixels.append(Pixel(x + 9, y + 10, *self.red))
-        pixels.append(Pixel(x + 10, y + 10, *self.flesh))
-        pixels.append(Pixel(x + 11, y + 10, *self.flesh))
+        pixels.append(Pixel(x + 9, y + 10, *self.blue))
+        pixels.append(Pixel(x + 10, y + 10, *self.blue))
+        pixels.append(Pixel(x + 11, y + 10, *self.blue))
+        pixels.append(Pixel(x + 12, y + 10, *self.blue))
+        pixels.append(Pixel(x + 13, y + 10, *self.brown))
+        pixels.append(Pixel(x + 14, y + 10, *self.brown))
 
 
-        pixels.append(Pixel(x + 0, y + 11, *self.flesh))
-        pixels.append(Pixel(x + 1, y + 11, *self.flesh))
-        pixels.append(Pixel(x + 2, y + 11, *self.flesh))
-        pixels.append(Pixel(x + 3, y + 11, *self.blue))
+        pixels.append(Pixel(x + 2, y + 11, *self.brown))
+        pixels.append(Pixel(x + 3, y + 11, *self.brown))
         pixels.append(Pixel(x + 4, y + 11, *self.blue))
         pixels.append(Pixel(x + 5, y + 11, *self.blue))
         pixels.append(Pixel(x + 6, y + 11, *self.blue))
         pixels.append(Pixel(x + 7, y + 11, *self.blue))
         pixels.append(Pixel(x + 8, y + 11, *self.blue))
-        pixels.append(Pixel(x + 9, y + 11, *self.flesh))
-        pixels.append(Pixel(x + 10, y + 11, *self.flesh))
-        pixels.append(Pixel(x + 11, y + 11, *self.flesh))
+        pixels.append(Pixel(x + 9, y + 11, *self.blue))
+        pixels.append(Pixel(x + 10, y + 11, *self.blue))
+        pixels.append(Pixel(x + 11, y + 11, *self.blue))
+        pixels.append(Pixel(x + 12, y + 11, *self.blue))
+        pixels.append(Pixel(x + 13, y + 11, *self.brown))
+        pixels.append(Pixel(x + 14, y + 11, *self.brown))
 
 
-        pixels.append(Pixel(x + 0, y + 12, *self.flesh))
-        pixels.append(Pixel(x + 1, y + 12, *self.flesh))
-        pixels.append(Pixel(x + 2, y + 12, *self.blue))
+        pixels.append(Pixel(x + 1, y + 12, *self.brown))
+        pixels.append(Pixel(x + 2, y + 12, *self.brown))
+        pixels.append(Pixel(x + 3, y + 12, *self.brown))
         pixels.append(Pixel(x + 3, y + 12, *self.blue))
         pixels.append(Pixel(x + 4, y + 12, *self.blue))
         pixels.append(Pixel(x + 5, y + 12, *self.blue))
         pixels.append(Pixel(x + 6, y + 12, *self.blue))
         pixels.append(Pixel(x + 7, y + 12, *self.blue))
         pixels.append(Pixel(x + 8, y + 12, *self.blue))
-        pixels.append(Pixel(x + 9, y + 12, *self.blue))
-        pixels.append(Pixel(x + 10, y + 12, *self.flesh))
-        pixels.append(Pixel(x + 11, y + 12, *self.flesh))
 
+        pixels.append(Pixel(x + 1, y + 13, *self.brown))
+        pixels.append(Pixel(x + 2, y + 13, *self.brown))
 
-        pixels.append(Pixel(x + 2, y + 13, *self.blue))
-        pixels.append(Pixel(x + 3, y + 13, *self.blue))
-        pixels.append(Pixel(x + 4, y + 13, *self.blue))
-        pixels.append(Pixel(x + 7, y + 13, *self.blue))
-        pixels.append(Pixel(x + 8, y + 13, *self.blue))
-        pixels.append(Pixel(x + 9, y + 13, *self.blue))
-
-
-        # Feet
-        pixels.append(Pixel(x + 1,  y + 14, *self.brown))
-        pixels.append(Pixel(x + 2,  y + 14, *self.brown))
-        pixels.append(Pixel(x + 3,  y + 14, *self.brown))
-        pixels.append(Pixel(x + 8,  y + 14, *self.brown))
-        pixels.append(Pixel(x + 9,  y + 14, *self.brown))
-        pixels.append(Pixel(x + 10,  y + 14, *self.brown))
-
-
-        pixels.append(Pixel(x + 0,  y + 15, *self.brown))
-        pixels.append(Pixel(x + 1,  y + 15, *self.brown))
-        pixels.append(Pixel(x + 2,  y + 15, *self.brown))
-        pixels.append(Pixel(x + 3,  y + 15, *self.brown))
-        pixels.append(Pixel(x + 8,  y + 15, *self.brown))
-        pixels.append(Pixel(x + 9,  y + 15, *self.brown))
-        pixels.append(Pixel(x + 10,  y + 15, *self.brown))
-        pixels.append(Pixel(x + 11,  y + 15, *self.brown))
+        # Jumping Mario is 17 pixels wide. With 8 as the mid-point
+        # flip all pixels around the 8 pixel mark to turn him around
+        if inverse:
+            left = x - 2
+            right = left + 17
+            mid = left + 8
+            for p in pixels:
+                if p.x < mid:
+                    distance = mid - p.x
+                    p.x = p.x + distance * 2
+                elif p.x > mid:
+                    distance = p.x - mid
+                    p.x = p.x - distance * 2
 
         return pixels
 
-    def getStanding(self, x, y):
+    def getStanding(self, x, y, inverse = False):
         pixels = []
         pixels.append(Pixel(x + 4,  y + 0, *self.red))
         pixels.append(Pixel(x + 5,  y + 0, *self.red))
@@ -406,6 +426,21 @@ class Mario(SampleBase):
         pixels.append(Pixel(x + 9,  y + 15, *self.brown))
         pixels.append(Pixel(x + 10,  y + 15, *self.brown))
         pixels.append(Pixel(x + 11,  y + 15, *self.brown))
+
+        # Standing Mario is 12 pixels wide. With 6 as the mid-point
+        # flip all pixels around the 6 pixel mark to turn him around
+        if inverse:
+            left = x 
+            right = left + 12
+            mid = left + 6
+            for p in pixels:
+                if p.x < mid:
+                    distance = mid - p.x
+                    p.x = p.x + distance * 2
+                elif p.x > mid:
+                    distance = p.x - mid
+                    p.x = p.x - distance * 2
+
         return pixels
             
 
